8a5574f6ebe8c6c7e0df6c83babef79d2ce13197
hisahito <hisahito706@gmail.com>
2022-05-07 01:28:45 -0500

    Adding a new model in schema.prisma. A new migration. Adding new registers in seed.js reflected in our db and new table with the name: newTable

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,77 +3,41 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
+    const missionCommander1 = await prisma.newTable.upsert({
+      where: { name: 'Carlo' },
       update: {},
       create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
+        name: 'Carlo',
+				lang: 'Estevez',
+				missionCommander: 'Backend',
+        enrollments: 1
       },
     });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
+    const missionCommander2 = await prisma.newTable.upsert({
+      where: { name: 'Fer' },
       update: {},
       create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
+        name: 'Fer',
+				lang: 'Martinez',
+				missionCommander: 'onboarding',
+        enrollments: 2
       },
     });
 
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
+    const missionCommander3 = await prisma.newTable.upsert({
+      where: { name: 'Rodrigo' },
       update: {},
       create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
+        name: 'Rodrigo',
+				lang: 'Fernandez',
+				missionCommander: 'Frontend',
+        enrollments: 3
       },
     });
 
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-
-    const woopa4 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 4' },
-        update: {},
-        create: {
-            name: 'Woopa 4',
-                    username: 'ajolonauta4',
-                    mission: 'Java'
-        },
-    });
-
-    const woopa5 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 5' },
-        update: {},
-        create: {
-            name: 'Woopa 5',
-                    username: 'ajolonauta5',
-                    mission: 'Node'
-        },
-    });
-
-    const woopa6 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 6' },
-        update: {},
-        create: {
-            name: 'Woopa 6',
-                    username: 'ajolonauta6',
-                    mission: 'Java'
-        },
-    });
-
-    console.log('Create 7 explorers');
+    
+    console.log('Create 3 missionsCommanders');
   } catch(e) {
     console.error(e);
     process.exit(1);
